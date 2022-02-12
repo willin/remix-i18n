2caa6d2707121d6b644e464a69eb0d64294b04cd
Willin Wang <willin@willin.org>
2022-02-12 18:30:12 +0800

    fix: :bug: fix node version

--- documents/app/entry.server.tsx
+++ documents/app/entry.server.tsx
@@ -10,17 +10,11 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-<<<<<<< HEAD:documents/app/entry.server.tsx
   const locale = getLocale(new URL(request.url).pathname);
   i18n.locale(locale);
 
   const markup = renderToString(
     <I18nProvider i18n={i18n}>
-=======
-  let locale = getLocale(new URL(request.url).pathname);
-  const markup = renderToString(
-    <I18nProvider fallback='zh' locale={locale}>
->>>>>>> fc247859ce627ebd230205053f44b200bd6e46eb:app/entry.server.tsx
       <RemixServer context={remixContext} url={request.url} />
     </I18nProvider>
   );
