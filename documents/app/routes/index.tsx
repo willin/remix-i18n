3f432eefd20a5a6b1ad0d443edc390c370b724bc
Willin Wang <willin@willin.org>
2023-09-19 21:28:24 +0800

    fix: compatibility_flags

--- documents/app/routes/index.tsx
+++ documents/app/routes/index.tsx
@@ -2,7 +2,7 @@ import { LoaderFunction, redirect } from '@remix-run/cloudflare';
 import { pick } from 'accept-language-parser';
 import { i18n } from '../i18n';
 
-export const loader: LoaderFunction = ({ request }) => {
+export const loader: LoaderFunction = async ({ request }) => {
   const locale =
     pick(i18n.supportedLanguages, request.headers.get('Accept-Language')) ||
     i18n.fallbackLng;
